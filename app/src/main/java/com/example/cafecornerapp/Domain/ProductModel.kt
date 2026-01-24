15bf88601f290cbba7b1eb18159bf906bfe07408
Opjasson <azmitheusos333@gmail.com>
2026-01-24 20:40:04 +0700

    Domain : add product model

---
+++ app/src/main/java/com/example/cafecornerapp/Domain/ProductModel.kt
@@ -0,0 +1,10 @@
+package com.example.cafecornerapp.Domain
+
+data class ProductModel(
+    var nama_product : String = "",
+    var harga_product : Long = 0,
+    var deskripsi_product : String = "",
+    var imgUrl : String = "",
+    var kategori_product : String = "",
+    var promo : Boolean = false,
+)
