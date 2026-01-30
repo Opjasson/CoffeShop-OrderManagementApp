80cf319f3a1cd60daff811c7470a26b19be11f7e
Opjasson <azmitheusos333@gmail.com>
2026-01-30 11:16:22 +0700

    LaporanActivity : done setting print data function

--- app/src/main/java/com/example/cafecornerapp/Activity/LaporanTransactionActivity.kt
+++ app/src/main/java/com/example/cafecornerapp/Activity/LaporanTransactionActivity.kt
@@ -1,24 +1,36 @@
 package com.example.cafecornerapp.Activity
 
+import android.content.Context
 import android.content.Intent
+import android.graphics.Canvas
 import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.pdf.PdfDocument
 import android.os.Bundle
+import android.os.Environment
+import android.text.TextPaint
+import android.text.TextUtils
 import android.util.Log
 import android.view.Gravity
 import android.widget.TableRow
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.FileProvider
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.drawerlayout.widget.DrawerLayout
+import com.example.cafecornerapp.Domain.LaporanModel
 import com.example.cafecornerapp.Helper.ConvertDateTime
 import com.example.cafecornerapp.R
 import com.example.cafecornerapp.ViewModel.TransaksiViewModel
 import com.example.cafecornerapp.databinding.ActivityLaporanTransactionBinding
 import com.google.android.material.datepicker.MaterialDatePicker
 import com.google.android.material.textfield.TextInputEditText
+import java.io.File
+import java.io.FileOutputStream
 import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.Locale
@@ -126,12 +138,12 @@ class LaporanTransactionActivity : AppCompatActivity() {
                 }
                 Log.d("LISTBARANGMASUK", list.toString())
 
-//                binding.cetakBtn.setOnClickListener {
-//                    generatePdf(this, list)
-//                }
-
+                binding.cetakBtn.setOnClickListener {
+                    generatePdf(this, list)
+                }
 
                 var totalKeseluruhan : Long = 0
+                var nomor : Long = 1
                 list.forEachIndexed { index2, prod ->
 
                     prod.cartItems.forEachIndexed {
@@ -142,7 +154,7 @@ class LaporanTransactionActivity : AppCompatActivity() {
                         val subtotal = item.jumlah * item.harga
                         totalKeseluruhan += subtotal
 
-                        row.addView(createCell((index + 1).toString(), Gravity.CENTER))
+                        row.addView(createCell(nomor.toString(), Gravity.CENTER))
                         row.addView(createCell(item.createdAt.toString(), Gravity.CENTER))
                         row.addView(createCell(item.nama.toString(), Gravity.CENTER))
                         row.addView(createCell(item.jumlah.toString(), Gravity.START))
@@ -150,7 +162,7 @@ class LaporanTransactionActivity : AppCompatActivity() {
                         row.addView(createCell(
                             subtotal.toString(), Gravity.END)
                         )
-
+                        nomor++
                         table.addView(row)
                     }
                 }
@@ -169,5 +181,155 @@ class LaporanTransactionActivity : AppCompatActivity() {
             setPadding(8, 8, 8, 8)
             setBackgroundResource(android.R.drawable.editbox_background)
         }
+    }
+
+    fun generatePdf(context: Context, data: List<LaporanModel>) {
+
+        val pdfDocument = PdfDocument()
+
+        val textPaint = TextPaint().apply {
+            textSize = 12f
+            isAntiAlias = true
+        }
+
+        val titlePaint = Paint().apply {
+            textSize = 16f
+            isFakeBoldText = true
+            isAntiAlias = true
+        }
+
+        val linePaint = Paint().apply {
+            style = Paint.Style.STROKE
+            strokeWidth = 1f
+        }
+
+        // Ukuran A4 (pt)
+        val pageWidth = 595
+        val pageHeight = 842
+
+        // Layout table
+        val startX = 30f
+        val startY = 100f
+        val rowHeight = 28f
+        val bottomMargin = 800f
+
+        // Kolom X
+        val colNo = startX
+        val colNama = startX + 40
+        val colAwal = startX + 180
+        val colMasuk = startX + 270
+        val colKeluar = startX + 360
+        val colAkhir = startX + 450
+
+        fun drawTextSafe(
+            canvas: Canvas,
+            text: String,
+            x: Float,
+            y: Float,
+            maxWidth: Float
+        ) {
+            val safeText = TextUtils.ellipsize(
+                text,
+                textPaint,
+                maxWidth,
+                TextUtils.TruncateAt.END
+            ).toString()
+
+            canvas.drawText(safeText, x, y, textPaint)
+        }
+
+        fun drawHeader(canvas: Canvas, y: Float) {
+            drawTextSafe(canvas, "No", colNo, y, 30f)
+            drawTextSafe(canvas, "Tanggal", colNama, y, 120f)
+            drawTextSafe(canvas, "Nama Menu", colAwal, y, 70f)
+            drawTextSafe(canvas, "Jumlah", colMasuk, y, 70f)
+            drawTextSafe(canvas, "Harga", colKeluar, y, 70f)
+            drawTextSafe(canvas, "Total", colAkhir, y, 70f)
+
+            canvas.drawLine(20f, y + 5f, pageWidth - 20f, y + 5f, linePaint)
+        }
+
+        var pageNumber = 1
+        var pageInfo = PdfDocument.PageInfo.Builder(pageWidth, pageHeight, pageNumber).create()
+        var page = pdfDocument.startPage(pageInfo)
+        var canvas = page.canvas
+
+        // Judul
+        canvas.drawText("Laporan Data Penjualan", 40f, 50f, titlePaint)
+
+        var y = startY
+        drawHeader(canvas, y)
+        y += rowHeight
+
+
+            var totalKeseluruhan : Long = 0
+            var nomor : Long = 1
+
+        data.forEach { laporan ->
+
+            laporan.cartItems.forEach { item ->
+
+                // pindah halaman
+                if (y > bottomMargin) {
+                    pdfDocument.finishPage(page)
+
+                    pageNumber++
+                    pageInfo = PdfDocument.PageInfo.Builder(pageWidth, pageHeight, pageNumber).create()
+                    page = pdfDocument.startPage(pageInfo)
+                    canvas = page.canvas
+
+                    canvas.drawText("Laporan Data Penjualan", 40f, 50f, titlePaint)
+
+                    y = startY
+                    drawHeader(canvas, y)
+                    y += rowHeight
+                }
+
+                val subtotal = item.jumlah * item.harga
+                totalKeseluruhan += subtotal
+
+                drawTextSafe(canvas, nomor.toString(), colNo, y, 30f)
+                drawTextSafe(canvas, item.createdAt ?: "-", colNama, y, 120f)
+                drawTextSafe(canvas, item.nama, colAwal, y, 70f)
+                drawTextSafe(canvas, item.jumlah.toString(), colMasuk, y, 70f)
+                drawTextSafe(canvas, localIDR.formatRupiah(item.harga), colKeluar, y, 70f)
+                drawTextSafe(canvas, localIDR.formatRupiah(subtotal), colAkhir, y, 70f)
+
+                y += rowHeight
+                nomor++
+            }
+        }
+//            binding.totalKeseluruhanTxt.text =
+        y += 10f
+        canvas.drawLine(20f, y, pageWidth - 20f, y, linePaint)
+        y += 25f
+
+        canvas.drawText(
+            "TOTAL KESELURUHAN : ${localIDR.formatRupiah(totalKeseluruhan).toString()}",
+            colAkhir - 200,
+            y,
+            titlePaint
+        )
+
+        pdfDocument.finishPage(page)
+
+        val file = File(context.getExternalFilesDir(null), "laporan_penjualan.pdf")
+        pdfDocument.writeTo(FileOutputStream(file))
+        pdfDocument.close()
+
+        Toast.makeText(context, "PDF berhasil dibuat", Toast.LENGTH_SHORT).show()
+
+        val uri = FileProvider.getUriForFile(
+            context,
+            "${context.packageName}.provider",
+            file
+        )
+
+        val intent = Intent(Intent.ACTION_VIEW).apply {
+            setDataAndType(uri, "application/pdf")
+            flags = Intent.FLAG_GRANT_READ_URI_PERMISSION or Intent.FLAG_ACTIVITY_NEW_TASK
+        }
+
+        context.startActivity(intent)
     }
 }
